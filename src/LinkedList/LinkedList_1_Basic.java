ecfd0f72a5ffa2ba8f1c5a37e62ae7ca46fe0a7a
raj4off <raj4off@gmail.com>
2026-04-07 09:06:07 +0530

    linked list basic

--- src/LinkedList/LinkedList_1_Basic.java
+++ src/LinkedList/LinkedList_1_Basic.java
@@ -1,49 +1,121 @@
 package LinkedList;
 
-//java -cp out src/LinkedList/LinkedList_1_Basic.java
+class Node{
+    int val;
+    Node next;
+
+    Node(int val){
+        this.val = val;
+        this.next = null;
+    }
+
+    Node(int val, Node next){
+        this.val = val;
+        this.next = next;
+    }
+}
+
 public class LinkedList_1_Basic {
 
-    static class Node{
-        int value;
-        Node next;
-        Node(int value){
-            this.value = value;
-            this.next = null;
-        }
-    }
+   private static Node convertArrayToLinkedList(int[] arr){
+       Node head = null;
+       Node cur = null;
+       for(int i : arr){
+           if(head==null){
+               head = new Node(i);
+               cur = head;
+           } else {
+               Node temp = new Node(i);
+               cur.next = temp;
+               cur = temp;
+           }
+       }
+       return head;
+   }
+
+   private static void traverseLinkedList(Node head){
+       Node cur = head;
+       while(cur != null){
+           System.out.print(cur.val+" ");
+           cur = cur.next;
+       }
+       System.out.println();
+   }
+
+   private static int findLengthOfLinkedList(Node head){
+       int len = 0;
+       Node cur = head;
+       while(cur!=null){
+           len++;
+           cur=cur.next;
+       }
+       return len;
+   }
+
+   private static Node removeHead(Node head){
+       if(head == null) return null;
+       return head.next;
+   }
+
+   private static Node removeTail(Node head){
+       if(head == null || head.next == null) return null;
+       Node cur = head;
+
+       while(cur.next.next != null){
+           cur = cur.next;
+       }
+       cur.next = null;
+       return head;
+   }
+
+   private static Node removeKthElement(Node head, int k){
+       Node cur = head;
+       Node prev = cur;
+       if(k == 1) return head.next;
+       while(cur != null){
+           k--;
+           if(k==0){
+               prev.next = cur.next;
+           }
+           prev = cur;
+           cur = cur.next;
+       }
+       return head;
+   }
+
+   private static Node insertElementAtK(Node head, int k, int val){
+       Node cur = head;
+       if(k == 1) return new Node(val, head);
+       Node node = new Node(val);
+       while(cur != null){
+           k--;
+           if(k==1){
+               node.next = cur.next;
+               cur.next = node;
+               break;
+           }
+           cur = cur.next;
+       }
+       return head;
+   }
 
     public static void main(String[] args) {
         int[] arr = new int[]{1,2,3,4,5,6,7};
-        int k = 8;
-        int len = arr.length;
-        Node head = null;
-        Node current = null;
+        Node head = convertArrayToLinkedList(arr);
 
-        for(int i: arr){
-            if(head==null){
-                current = new Node(i);
-                head = current;
-            } else{
-                current.next = new Node(i);
-                current = current.next;
-            }
-        }
-        current.next = head;
+        traverseLinkedList(head);
+        System.out.println("Length of Linked list: "+findLengthOfLinkedList(head));
 
-        Node temp = head;
-        int i = (len-(k%len))-1;
-        while(temp!=null && i>0){
-            temp = temp.next;
-            i--;
-        }
-        head = temp.next;
-        temp.next = null;
+        head = removeHead(head);
+        traverseLinkedList(head);
 
-        temp = head;
-        while(temp!=null){
-            System.out.print(temp.value + " ");
-            temp = temp.next;
-        }
+        head = removeTail(head);
+        traverseLinkedList(head);
+
+        head = removeKthElement(head, 5);
+        traverseLinkedList(head);
+
+        head = insertElementAtK(head, 5, 11);
+        traverseLinkedList(head);
     }
-    
 }
